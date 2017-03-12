fc212fd803706a1179ab8bfca13f72883c49c95e
nitin <nitin.iitd16@gmail.com>
2017-03-12 12:36:21 +0530

    separate out debug function

--- pipeline.py
+++ pipeline.py
@@ -9,25 +9,29 @@ lane_finder = LaneFinder()
 
 prefix='../CarND-Advanced-Lane-Lines/'
 
-def process_image(oimg):
+def debug_image(omig):
     uimg = undistort.undistort(oimg)
     _, _, _, timg = threshold.threshold(uimg)
     wimg = topview.warp(timg)
     #result = np.dstack((timg, timg, timg))
     #result = np.dstack((wimg, wimg, wimg))
-    #plt.imshow(result)
-    #plt.show()
     left_fit, right_fit, roc, dfc, ly, lx, ry, rx = lane_finder.find(wimg)
 
     #ploty = np.linspace(0, wimg.shape[0]-1, wimg.shape[0]).astype(int)
     #left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
     #right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
     #result = np.dstack((wimg, wimg, wimg))
-    #result[ly, lx] = [255, 0, 0]
-    #result[ry, rx] = [255, 0, 0]
+    result[ly, lx] = [255, 0, 0]
+    result[ry, rx] = [255, 0, 0]
     #result[ploty, left_fitx.astype(int)] = [255, 255, 0]
     #result[ploty, right_fitx.astype(int)] = [255, 255, 0]
-    #valid_index =
+    return result
+
+def process_image(oimg):
+    uimg = undistort.undistort(oimg)
+    _, _, _, timg = threshold.threshold(uimg)
+    wimg = topview.warp(timg)
+    left_fit, right_fit, roc, dfc, ly, lx, ry, rx = lane_finder.find(wimg)
     result = draw.draw(uimg, wimg, left_fit, right_fit, roc, dfc, 
             ly, lx, ry, rx, False)
     return result
